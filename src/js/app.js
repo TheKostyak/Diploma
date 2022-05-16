15b15b1d1859588047b4944acf14247d56e691ed
TheKostyak <kostaa8@gmail.com>
2022-05-16 20:38:12 +0300

    added burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,18 @@ mediumZoom(".catalog__item_image", {
     margin: 100,
 });
 
-const dropDown = document.getElementById('dropDown');
+const dropDown = document.getElementById("dropDown");
 const navDropdown = document.getElementsByClassName("nav__dropdown");
 navDropdown[0].addEventListener("click", () => {
     dropDown.classList.toggle("nav__dropdown_active");
     navDropdown[0].classList.toggle("nav__dropdown_active");
+
+});
+
+const burger = document.getElementById("burger");
+const navLinks = document.getElementsByClassName("nav__links")
+burger.addEventListener("click", () => {
+    burger.classList.toggle("burger__active");
+    navLinks[0].classList.toggle("nav__links_active");
 
 });
